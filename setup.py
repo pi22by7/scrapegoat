1a77a257ed7bc2912b7777574071db35b4312455
pi22by7 <piyushairani123@outlook.com>
2023-07-02 22:30:51 +0530

    First build!

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Scrapegoat',
+    version='0.2.0207',
+    packages=[''],
+    url='',
+    license='GPLv3',
+    author='Piyush Airani',
+    author_email='piyushairani@outlook.com',
+    description='A Python web scraper built with BeautifulSoup for extracting data from websites, handling links and '
+                'pagination, and saving results to CSV.'
+)
